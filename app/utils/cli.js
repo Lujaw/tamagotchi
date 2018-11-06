71dee63e7a302635b85720bd6645c9106bb4edf3
Luja Shrestha <lshrestha@woolworths.com.au>
2018-11-06 22:00:11 +1100

    feat: adding the cli element

--- app/utils/cli.js
+++ app/utils/cli.js
@@ -1,38 +1,11 @@
 const prompt = require('async-prompt');
 const R = require('ramda');
-const Re = require('ramda-extension');
 
-
-const { actionList } = require("../tamagotchi/actions");
-
-const mapIndexed = R.addIndex(R.map);
-
-const indexedList = mapIndexed((val, idx) => `${idx + 1}-> ${Re.toPascalCase(val)}`);
-
-const indexedActionList = indexedList(actionList).join('\n');
-
-
-const promptUser = async () => {
-  console.log(`What would you like to do? \n${indexedActionList}` );
-  return await prompt(`Enter number [1-${actionList.length}]: `);
+const promptUser = (promptText, maxNumber) => async (_)  => {
+  console.log(`What would you like to do? \n${promptText}` );
+  return await prompt(`Enter number [1-${maxNumber}]: `);
 }
 
-
-const getUserAction = index => index;
-
-(async () => {
-  try {
-    let input = await promptUser();
-    console.log( input)
-    while(input <= actionList.length ){
-      input = await promptUser();
-    }
-    console.log('Thank you')
-    process.exit();
-
-
-
-  } catch (e) {
-    console.log('cli.js#29 --->', e)
-  }
-})()
+module.exports = {
+  promptUser
+}
